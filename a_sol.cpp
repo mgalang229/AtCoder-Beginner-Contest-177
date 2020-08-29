d27e6f49fb3dd7bd5890367ff227c92ad28f3b61
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-08-29 21:49:15 +0800

    Create a_sol.cpp

---
+++ a_sol.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+int main() {
+	float d, t, s;
+	cin >> d >> t >> s;
+	if(d/s<=t)
+		cout << "Yes";
+	else
+		cout << "No";
+	cout << "\n";
+}
